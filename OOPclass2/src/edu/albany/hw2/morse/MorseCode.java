5e4c8d91afde62ed3db9a70894ec070e2404162c
france le <fleducation001@gmail.com>
2018-02-16 21:59:29 -0500

    Integrated HashMap, Replaced Case Statements Looks better
    Tested for both, might require more testing.

--- OOPclass2/src/edu/albany/hw2/morse/MorseCode.java
+++ OOPclass2/src/edu/albany/hw2/morse/MorseCode.java
@@ -1,20 +1,50 @@
 package edu.albany.hw2.morse;
 
+import java.util.HashMap;
+
+
 public class MorseCode {
-	private StringBuffer sb;
+	private HashMap<String, String> codeMap; 
 	
 	public MorseCode() {
-		sb = new StringBuffer("");
+		codeMap = new HashMap<String, String>();
+		//Numbers
+		codeMap.put("0", "-----"); codeMap.put("1", ".----"); codeMap.put("2","..---"); codeMap.put("3", "...--");
+		codeMap.put("4", "....-"); codeMap.put("5","....."); codeMap.put("6","-...."); codeMap.put("7","--...");
+		codeMap.put("8","---.."); codeMap.put("9","----.");
+		
+		//Special characters
+		codeMap.put(" ", " "); codeMap.put(",", "--..--"); codeMap.put(".",".-.-.-"); codeMap.put("?", "..--..");
+		
+		//Letters
+		codeMap.put("A", ".-"); codeMap.put("B", "-..."); codeMap.put("C", "-.-."); codeMap.put("D", "-..");
+		codeMap.put("E", "."); codeMap.put("F", "..-."); codeMap.put("G", "--."); codeMap.put("H", "....");
+		codeMap.put("I", ".."); codeMap.put("J", ".---");codeMap.put("K","-.-"); codeMap.put("L",".-..");
+		codeMap.put("M","--"); codeMap.put("N","-."); codeMap.put("O","---"); codeMap.put("P", ".--."); 
+		codeMap.put("Q", "--.-");codeMap.put("R", ".-."); codeMap.put("S", "..."); codeMap.put("T","-");
+		codeMap.put("U","..-"); codeMap.put("V","...-"); codeMap.put("W",".--"); codeMap.put("X","-..-");
+		codeMap.put("Y", "-.--");codeMap.put("Z", "--..");		
 	}
 	
-	public void appendString(Morse m) {
-		sb.append(m.getStrCode());
+	public HashMap<String,String> getCodeMap(){
+		return codeMap;
 	}
 	
-	public String getMorseCodeString() {
-		return sb.toString();
-	}
+
 	
 	
 	
 }
+
+
+//public MorseCode() {
+//sb = new StringBuffer("");
+//}
+//
+//public void appendString(Morse m) {
+//sb.append(m.getStrCode());
+//}
+//
+//public String getMorseCodeString() {
+//return sb.toString();
+//}
--- OOPclass2/src/edu/albany/hw2/morse/Parser.java
+++ OOPclass2/src/edu/albany/hw2/morse/Parser.java
@@ -1,193 +1,215 @@
 package edu.albany.hw2.morse;
 
-import java.util.Scanner;
+import java.util.HashMap;
+
 
 //the purpose of this class is to break down the user input into easy to handle strings
 //command line args may or may not contain space; after every word, you need to add a space
 		//Example input ./javafile Hello " " World.		------> mmmm   mmmm
 		//Example input ./javafile Hello World			------> mmmm mmmm
 public class Parser {
-	private String stringToParse;
-	private MorseCode morseCode;
+	private String outputString;
+	private StringBuffer sb;
+	private MorseCode codes;
 
-	
 	public Parser() {
-		this.morseCode = new MorseCode();
+		sb = new StringBuffer();
+		codes = new MorseCode();
 	}
 	
 	public void parseCmd(String[] args) {
 		for(String word: args) {				
-			if(word.compareTo(" ")==0) {
-				morseCode.appendString(Morse.SPACE); }
+			if(word.compareTo(" ")==0) {	
+				sb.append(" ");
+				 }
 			else { 
 				convertStringToCode(word);					//this will past each argument word of word and add additional spacing if not available
-				morseCode.appendString(Morse.SPACE);			
-				}
+				sb.append(" ");	}			
 		}//for
 	}//end of method
 
-	//splits userinput into an array and passes into array parse handler
 	public void parseUserInput(String userInput) {
-		parseCmd(userInput.split("\\s")); 
+			convertStringToCode(userInput); 
+		
+		
 	}
 	
 	//this allows the parser to return the morseCode Object
 	public String getMorseCode() {
-		return morseCode.getMorseCodeString();
+		return sb.toString();
 	}
 	
-	//this will be parsing individual words
+	
 	public void convertStringToCode(String str) {
-		//this is where the case statements go
-		//morseCode.appendString("**arg**");
-		//CONCERN: having so many nested-for-loops will make the performance of this program really slow. I know it.
-		//loop through the word
+		HashMap<String, String> codeMap = codes.getCodeMap();
 		for(int i = 0; i < str.length(); i++) {
 			char letter = str.charAt(i);
 			String input = Character.toString(letter).toUpperCase();
-			
-			switch(input) {
-			case "A": 
-				morseCode.appendString(Morse.A);
-				break;
-			case "B":
-				morseCode.appendString(Morse.B);
-				break;
-			case "C": 
-				morseCode.appendString(Morse.C);
-				break;
-			case "D":
-				morseCode.appendString(Morse.D);
-				break;
-			case "E": 
-				morseCode.appendString(Morse.E);
-				break;
-			case "F":
-				morseCode.appendString(Morse.F);
-				break;
-			case "G": 
-				morseCode.appendString(Morse.G);
-				break;
-			case "H":
-				morseCode.appendString(Morse.H);
-				break;
-			case "I": 
-				morseCode.appendString(Morse.I);
-				break;
-			case "J":
-				morseCode.appendString(Morse.J);
-				break;
-			case "K": 
-				morseCode.appendString(Morse.K);
-				break;
-			case "L":
-				morseCode.appendString(Morse.L);
-				break;
-			case "M": 
-				morseCode.appendString(Morse.M);
-				break;
-			case "N":
-				morseCode.appendString(Morse.N);
-				break;
-			case "O": 
-				morseCode.appendString(Morse.O);
-				break;
-			case "P":
-				morseCode.appendString(Morse.P);
-				break;
-			case "Q": 
-				morseCode.appendString(Morse.Q);
-				break;
-			case "R":
-				morseCode.appendString(Morse.R);
-				break;
-			case "S": 
-				morseCode.appendString(Morse.S);
-				break;
-			case "T":
-				morseCode.appendString(Morse.T);
-				break;
-			case "U": 
-				morseCode.appendString(Morse.U);
-				break;
-			case "V":
-				morseCode.appendString(Morse.V);
-				break;
-			case "W": 
-				morseCode.appendString(Morse.W);
-				break;
-			case "X":
-				morseCode.appendString(Morse.X);
-				break;
-			case "Y": 
-				morseCode.appendString(Morse.Y);
-				break;
-			case "Z":
-				morseCode.appendString(Morse.Z);
-				break;
-			case "0": 
-				morseCode.appendString(Morse.ZERO);
-				break;
-			case "1":
-				morseCode.appendString(Morse.ONE);
-				break;
-			case "2": 
-				morseCode.appendString(Morse.TWO);
-				break;
-			case "3":
-				morseCode.appendString(Morse.THREE);
-				break;
-			case "4": 
-				morseCode.appendString(Morse.FOUR);
-				break;
-			case "5":
-				morseCode.appendString(Morse.FIVE);
-				break;
-			case "6": 
-				morseCode.appendString(Morse.SIX);
-				break;
-			case "7":
-				morseCode.appendString(Morse.SEVEN);
-				break;
-			case "8": 
-				morseCode.appendString(Morse.EIGHT);
-				break;
-			case "9":
-				morseCode.appendString(Morse.NINE);
-				break;
-			case ".": 
-				morseCode.appendString(Morse.PERIOD);
-				break;
-			case ",":
-				morseCode.appendString(Morse.COMMA);
-				break;
-			case "?": 
-				morseCode.appendString(Morse.QUESTION);
-				break;
-			case " ": 
-				morseCode.appendString(Morse.SPACE);
-				break;
-			default:
-				System.out.println("Input not found");
-				break;
-			}
-			
-			
-			
-		}
-		
-		
-		
-		
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
+			if(input.equals(" ")) {										//This is a line to catch spaces for the user input option
+				sb.append(" ");
+				continue;
+				}
+			if(codeMap.get(input) != null) {
+				sb.append(codeMap.get(input));}
+			else
+				System.out.println(input + " does not exist in Morse Code mapping yet.");
+		}//for
+	}//convertString method
 
-}
+
+} //end of Class
+
+
+//public Parser() {
+//this.morseCode = new MorseCode();
+//}
+
+
+//if(word.compareTo(" ")==0) {
+//morseCode.appendString(Morse.SPACE); }
+//else { 
+//convertStringToCode(word);					//this will past each argument word of word and add additional spacing if not available
+//morseCode.appendString(Morse.SPACE);			
+//}
+
+
+//
+////this will be parsing individual words
+//public void convertStringToCode(String str) {
+//	//this is where the case statements go
+//	//morseCode.appendString("**arg**");
+//	//CONCERN: having so many nested-for-loops will make the performance of this program really slow. I know it.
+//	//loop through the word
+//	for(int i = 0; i < str.length(); i++) {
+//		char letter = str.charAt(i);
+//		String input = Character.toString(letter).toUpperCase();
+//		
+//		switch(input) {
+//		case "A": 
+//			morseCode.appendString(Morse.A);
+//			break;
+//		case "B":
+//			morseCode.appendString(Morse.B);
+//			break;
+//		case "C": 
+//			morseCode.appendString(Morse.C);
+//			break;
+//		case "D":
+//			morseCode.appendString(Morse.D);
+//			break;
+//		case "E": 
+//			morseCode.appendString(Morse.E);
+//			break;
+//		case "F":
+//			morseCode.appendString(Morse.F);
+//			break;
+//		case "G": 
+//			morseCode.appendString(Morse.G);
+//			break;
+//		case "H":
+//			morseCode.appendString(Morse.H);
+//			break;
+//		case "I": 
+//			morseCode.appendString(Morse.I);
+//			break;
+//		case "J":
+//			morseCode.appendString(Morse.J);
+//			break;
+//		case "K": 
+//			morseCode.appendString(Morse.K);
+//			break;
+//		case "L":
+//			morseCode.appendString(Morse.L);
+//			break;
+//		case "M": 
+//			morseCode.appendString(Morse.M);
+//			break;
+//		case "N":
+//			morseCode.appendString(Morse.N);
+//			break;
+//		case "O": 
+//			morseCode.appendString(Morse.O);
+//			break;
+//		case "P":
+//			morseCode.appendString(Morse.P);
+//			break;
+//		case "Q": 
+//			morseCode.appendString(Morse.Q);
+//			break;
+//		case "R":
+//			morseCode.appendString(Morse.R);
+//			break;
+//		case "S": 
+//			morseCode.appendString(Morse.S);
+//			break;
+//		case "T":
+//			morseCode.appendString(Morse.T);
+//			break;
+//		case "U": 
+//			morseCode.appendString(Morse.U);
+//			break;
+//		case "V":
+//			morseCode.appendString(Morse.V);
+//			break;
+//		case "W": 
+//			morseCode.appendString(Morse.W);
+//			break;
+//		case "X":
+//			morseCode.appendString(Morse.X);
+//			break;
+//		case "Y": 
+//			morseCode.appendString(Morse.Y);
+//			break;
+//		case "Z":
+//			morseCode.appendString(Morse.Z);
+//			break;
+//		case "0": 
+//			morseCode.appendString(Morse.ZERO);
+//			break;
+//		case "1":
+//			morseCode.appendString(Morse.ONE);
+//			break;
+//		case "2": 
+//			morseCode.appendString(Morse.TWO);
+//			break;
+//		case "3":
+//			morseCode.appendString(Morse.THREE);
+//			break;
+//		case "4": 
+//			morseCode.appendString(Morse.FOUR);
+//			break;
+//		case "5":
+//			morseCode.appendString(Morse.FIVE);
+//			break;
+//		case "6": 
+//			morseCode.appendString(Morse.SIX);
+//			break;
+//		case "7":
+//			morseCode.appendString(Morse.SEVEN);
+//			break;
+//		case "8": 
+//			morseCode.appendString(Morse.EIGHT);
+//			break;
+//		case "9":
+//			morseCode.appendString(Morse.NINE);
+//			break;
+//		case ".": 
+//			morseCode.appendString(Morse.PERIOD);
+//			break;
+//		case ",":
+//			morseCode.appendString(Morse.COMMA);
+//			break;
+//		case "?": 
+//			morseCode.appendString(Morse.QUESTION);
+//			break;
+//		case " ": 
+//			morseCode.appendString(Morse.SPACE);
+//			break;
+//		default:
+//			System.out.println("Input not found");
+//			break;
+//		}
+//		
+//		
+//		
+//	}
